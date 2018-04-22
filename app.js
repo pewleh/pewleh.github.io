8bf9a2b3264a3f67960f3069e1a44bbf8a1c9b8b
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2018-04-22 22:23:55 +0100

    styling skills container/ added functionality to buttons jq

--- app.js
+++ app.js
@@ -9,4 +9,43 @@ $(document).ready(function(){
       window.location.hash = target;
     });
   });
+  //skill switch ->
+  $('.middleBlock').hide();
+
+  $('#frontEnd').click(function() {
+    $('#small-logo').hide();
+    $('#frontBlock').show();
+    $('#backBlock').hide();
+    $('#softBlock').hide();
+  });
+
+  $('#backEnd').click(function() {
+    $('#frontBlock').hide();
+    $('#backBlock').show();
+    $('#softBlock').hide();
+  });
+
+  $('#softSkills').click(function() {
+    $('#frontBlock').hide();
+    $('#backBlock').hide();
+    $('#softBlock').show();
+
+  });
+  //skill switch end
+
+  //skill hover text ->
+  $(document).ready(function () {
+    $('#hover-image a img').hover(function () {
+      $(this).stop().animate({
+        opacity: .4
+      }, 200);
+      $('.text').removeClass('hide');
+    }, function () {
+      $(this).stop().animate({
+        opacity: 1
+      }, 500);
+      $('.text').addClass('hide');
+    });
+  });
+  //skill hover text end
 });
