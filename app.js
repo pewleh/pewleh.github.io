dcc06bef0e67b22be89d218aa20fc7876cedd7f4
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2018-04-25 13:16:19 +0100

    Started adding and styling experiece block

--- app.js
+++ app.js
@@ -9,48 +9,6 @@ $(document).ready(function(){
       window.location.hash = target;
     });
   });
-  //skill switch ->
-  $('.middleBlock').hide();
-
-  $('#frontEnd').click(function() {
-    $('#small-logo').hide();
-    $('#frontBlock').show();
-    $('#backBlock').hide();
-    $('#softBlock').hide();
-  });
-
-  $('#backEnd').click(function() {
-    $('#small-logo').hide();
-    $('#frontBlock').hide();
-    $('#backBlock').show();
-    $('#softBlock').hide();
-  });
-
-  $('#softSkills').click(function() {
-    $('#small-logo').hide();
-    $('#frontBlock').hide();
-    $('#backBlock').hide();
-    $('#softBlock').show();
-
-  });
-  //skill switch end
-
-  //skill hover text ->
-  $(document).ready(function () {
-    $('#hover-image a img').hover(function () {
-      $(this).stop().animate({
-        opacity: .4
-      }, 200);
-      $('.text').removeClass('hide');
-    }, function () {
-      $(this).stop().animate({
-        opacity: 1
-      }, 500);
-      $('.text').addClass('hide');
-    });
-  });
-  //end skill hover text
-
 
   //subheading changes
   const subheadings = [ 'Animal Hugger',
@@ -72,7 +30,82 @@ $(document).ready(function(){
       count = 0;
     }
   }
-
   changeSubHeading();
   //end subheading changes
+
+
+  //skill switch ->
+  $('.middleBlock').hide();
+
+  $('#frontEnd').click(function() {
+    console.log('click');
+    $('#small-logo').hide();
+    $('#frontBlock').show();
+    $('#backBlock').hide();
+    $('#softBlock').hide();
+  });
+
+  $('#backEnd').click(function() {
+    $('#small-logo').hide();
+    $('#frontBlock').hide();
+    $('#backBlock').show();
+    $('#softBlock').hide();
+  });
+
+  $('#softSkills').click(function() {
+    $('#small-logo').hide();
+    $('#frontBlock').hide();
+    $('#backBlock').hide();
+    $('#softBlock').show();
+  });
+  //skill switch end
+
+  //skill hover text ->
+  $(document).ready(function () {
+    $('#hover-image a img').hover(function () {
+      $(this).stop().animate({
+        opacity: .4
+      }, 200);
+      $('.text').removeClass('hide');
+    }, function () {
+      $(this).stop().animate({
+        opacity: 1
+      }, 500);
+      $('.text').addClass('hide');
+    });
+  });
+  //end skill hover text
+
+
+  //will need to refactor
+  //project switch->
+  $('#proj1').click(function() {
+    $('#firstBlock').show();
+    $('#secondBlock').hide();
+    $('#thirdBlock').hide();
+    $('#fourthBlock').hide();
+  });
+  $('#proj2').click(function() {
+    $('#firstBlock').hide();
+    $('#secondBlock').show();
+    $('#thirdBlock').hide();
+    $('#fourthBlock').hide();
+  });
+  $('#proj3').click(function() {
+    $('#firstBlock').hide();
+    $('#secondBlock').hide();
+    $('#thirdBlock').show();
+    $('#fourthBlock').hide();
+  });
+  $('#proj4').click(function() {
+    $('#firstBlock').hide();
+    $('#secondBlock').hide();
+    $('#thirdBlock').hide();
+    $('#fourthBlock').show();
+  });
+//end project switch
+
+
+
+
 });
